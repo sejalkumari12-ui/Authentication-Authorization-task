bbffe61ddf5c54263f009dffa5b1a4c31e4d12d0
sejalkumari12-ui <sejalkumari5679@gmail.com>
2026-01-20 13:34:09 +0530

    task setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// view engine
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// server
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
