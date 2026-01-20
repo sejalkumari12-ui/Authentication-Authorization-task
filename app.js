23295839a247165c4f6210dc619c13df307e8cb1
sejalkumari12-ui <sejalkumari5679@gmail.com>
2026-01-20 17:59:25 +0530

    bcrpt the password

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const userModel = require('./models/user')
+const bcrpt = require("bcrypt")
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -19,16 +20,25 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/create", async(req, res) => {
+
+app.post("/create", (req, res) => {
  let {username,email,password,age} = req.body
+
+ bcrpt.genSalt (10,(err,salt) =>{
+    bcrpt.hash(password , salt , async (err,hash) =>{
+         let createdUser = await userModel.create({
+            username,
+            email,
+            password : hash,
+             age,
+          })
+               res.send(createdUser)
+
+        })
+      })
+
  
- let createdUser = await userModel.create({
-    username,
-    email,
-    password,
-    age,
-  })
-  res.send(createdUser)
+
 });
 
 // server
